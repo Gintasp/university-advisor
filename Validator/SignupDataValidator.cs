a7156d26b999fa63d3aebacf6c782c3dc26f3a46
Laima Volungeviciute <laima.volungeviciute@gmail.com>
2019-09-20 21:44:53 +0300

    Made code more readable by deleting else cases
    Got rid of else cases.

--- Validator/SignupDataValidator.cs
+++ Validator/SignupDataValidator.cs
@@ -11,14 +11,15 @@ namespace Advisor.Validator
         public bool Validate(User user, string passConfirm)
         {
            this.user = user;
-            if ((CheckForWhiteSpaces(passConfirm) &&
+            if ((CheckForEmptyFields(passConfirm) &&
                  CheckEmailByRegex() &&
                  CheckPasswordByRegex()))
             {
                 errorMessage = null;
                 return true;
             }
-            else return false;
+
+            return false;
         }
         public string GetSignupDataVadilatorErrorMessage()
         {
@@ -28,35 +29,29 @@ namespace Advisor.Validator
         {
             Regex r = new Regex(@"^([\w\.]+)@([\w\-]+)((\.(\w){2,3})+)$");
             if (r.IsMatch(user.Email)) return true;
-            else
-            {
-                errorMessage = "Invalid email.";
-                return false;
-            }
+
+            errorMessage = "Invalid email.";
+            return false;
         }
         private bool CheckPasswordByRegex()
         {
             Regex r = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$");
             if(r.IsMatch(user.Password)) return true;
-            else 
-            {
-                errorMessage = "Password must contain at least eight characters, one uppercase letter, one lowercase letter and one number";
-                return false;
-            }
+
+            errorMessage = "Password must contain at least eight characters, one uppercase letter, one lowercase letter and one number";
+            return false;
 
         }
-        private bool CheckForWhiteSpaces(string passConfirm)
+        private bool CheckForEmptyFields(string passConfirm)
         {
             if (!(string.IsNullOrWhiteSpace(user.Email)
                 || string.IsNullOrWhiteSpace(user.Name)
                 || string.IsNullOrWhiteSpace(user.Password)
                 || string.IsNullOrWhiteSpace(passConfirm))) return true;
-
-            else
-            { 
-                errorMessage = "Please enter all required fields.";
-                return false;
-            }
+                      
+             errorMessage = "Please enter all required fields.";
+             return false;
+            
         } 
     }
 }
