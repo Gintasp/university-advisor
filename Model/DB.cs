9fbb7e66506eb179219a3fbb4374c1f3745f8c93
Gintautas Plonis <gplonis@gmail.com>
2019-09-22 19:30:11 +0300

    Create singleton DB instance

---
+++ Model/DB.cs
@@ -0,0 +1,21 @@
+﻿namespace Advisor.Model
+{
+    public sealed class DB
+    {
+        private static DatabaseContext instance = null;
+
+        private DB() { }
+        public static DatabaseContext Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DatabaseContext();
+                }
+
+                return instance;
+            }
+        }
+    }
+}
