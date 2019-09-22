039c67613a95b4d5c10817cc86e66659d570b511
Gintautas Plonis <gplonis@gmail.com>
2019-09-22 17:34:40 +0300

    Enable migrations

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Advisor.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Advisor.Model.DatabaseContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Advisor.Model.DatabaseContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
