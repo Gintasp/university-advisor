be509046ca9296abe62b33f3cd86e75e36211a58
Laima Volungeviciute <laima.volungeviciute@gmail.com>
2019-09-25 23:40:10 +0300

    Faculties list view
    Added faculites list view form

---
+++ View/FacultiesListView.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Advisor.View
+{
+    public partial class FacultiesListView : Form
+    {
+        public FacultiesListView()
+        {
+            InitializeComponent();
+        }
+        private void FacultiesListView_Load(object sender, EventArgs e)
+        {
+            //load FacultiesList
+        }
+
+        private void BtnAddFaculty_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtSearch_Enter(object sender, EventArgs e)
+        {
+            if(txtSearch.Text == "Search")
+            {
+                txtSearch.Text = "";
+                txtSearch.ForeColor = Color.Black;
+            }
+        }
+
+        private void TxtSearch_Leave(object sender, EventArgs e)
+        {
+            if (txtSearch.Text == "")
+            {
+                txtSearch.Text = "Search";
+                txtSearch.ForeColor = Color.Silver;
+            }
+
+        }
+    }
+}
