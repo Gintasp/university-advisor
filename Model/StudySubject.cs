6c11aa5e1ff8a45079936a9461f8ef9ee681a82b
povilassta <povilas1999@gmail.com>
2019-09-26 15:24:00 +0300

    Pridėta StudySubject klasė ir sukurtos Migrations į database.

---
+++ Model/StudySubject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Advisor.Model
+{
+    public class StudySubject
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+
+        public StudySubject()
+        {
+        }
+
+    }
+}
