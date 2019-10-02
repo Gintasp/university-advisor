a6e3010de9bf4b2567691d4e565d3ecfe18e624b
Gintautas Plonis <gplonis@gmail.com>
2019-10-02 10:50:03 +0300

    Fix null list bug, display name

--- Model/Lecturer.cs
+++ Model/Lecturer.cs
@@ -19,5 +19,10 @@ namespace Advisor.Model
         public Lecturer()
         {
         }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
--- View/List/LecturerListView.cs
+++ View/List/LecturerListView.cs
@@ -13,8 +13,8 @@ namespace Advisor.View
         {
             LecturerListController = lecturerListController;
             LecturerListController.LecturerListView = this;
-            LecturerList = LecturerData;
             InitializeComponent();
+            LecturerList = LecturerData;
         }
 
         private void OnSearchInputEnter(object sender, EventArgs e)
