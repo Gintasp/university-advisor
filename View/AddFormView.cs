9e128d56efb309fc7355407919efdc56f35a7489
Gintautas Plonis <gplonis@gmail.com>
2019-10-03 22:49:11 +0300

    Add view

---
+++ View/AddFormView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Advisor.View
+{
+    public partial class AddFormView : Form
+    {
+        public AddFormView()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAddButtonClick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
