7952c052fe96027f1c818e43b327eb14ff7c556b
Gintautas Plonis <gplonis@gmail.com>
2019-10-05 13:59:06 +0300

    Adjust styling of popup

--- View/PopupCourseComparison.Designer.cs
+++ View/PopupCourseComparison.Designer.cs
@@ -29,12 +29,14 @@
         private void InitializeComponent()
         {
             this.label3 = new System.Windows.Forms.Label();
-            this.FacultySelectBox1 = new System.Windows.Forms.ComboBox();
+            this.FacultySelectBox = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.UniversitySelectBox = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.CourseSelectBox1 = new System.Windows.Forms.ComboBox();
+            this.CourseSelectBox = new System.Windows.Forms.ComboBox();
             this.Choose = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label3
@@ -43,23 +45,24 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label3.Location = new System.Drawing.Point(127, 161);
+            this.label3.Location = new System.Drawing.Point(16, 103);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(77, 28);
+            this.label3.Size = new System.Drawing.Size(61, 21);
             this.label3.TabIndex = 17;
             this.label3.Text = "Faculty:";
             // 
-            // FacultySelectBox1
+            // FacultySelectBox
             // 
-            this.FacultySelectBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.FacultySelectBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FacultySelectBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.FacultySelectBox1.FormattingEnabled = true;
-            this.FacultySelectBox1.Location = new System.Drawing.Point(132, 189);
-            this.FacultySelectBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.FacultySelectBox1.Name = "FacultySelectBox1";
-            this.FacultySelectBox1.Size = new System.Drawing.Size(480, 36);
-            this.FacultySelectBox1.TabIndex = 16;
+            this.FacultySelectBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.FacultySelectBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FacultySelectBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.FacultySelectBox.FormattingEnabled = true;
+            this.FacultySelectBox.Location = new System.Drawing.Point(20, 130);
+            this.FacultySelectBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.FacultySelectBox.Name = "FacultySelectBox";
+            this.FacultySelectBox.Size = new System.Drawing.Size(361, 29);
+            this.FacultySelectBox.TabIndex = 16;
             // 
             // label1
             // 
@@ -67,9 +70,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label1.Location = new System.Drawing.Point(127, 49);
+            this.label1.Location = new System.Drawing.Point(16, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 28);
+            this.label1.Size = new System.Drawing.Size(83, 21);
             this.label1.TabIndex = 19;
             this.label1.Text = "University:";
             // 
@@ -79,10 +83,10 @@
             this.UniversitySelectBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.UniversitySelectBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.UniversitySelectBox.FormattingEnabled = true;
-            this.UniversitySelectBox.Location = new System.Drawing.Point(132, 79);
-            this.UniversitySelectBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.UniversitySelectBox.Location = new System.Drawing.Point(20, 48);
+            this.UniversitySelectBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.UniversitySelectBox.Name = "UniversitySelectBox";
-            this.UniversitySelectBox.Size = new System.Drawing.Size(480, 36);
+            this.UniversitySelectBox.Size = new System.Drawing.Size(361, 29);
             this.UniversitySelectBox.TabIndex = 18;
             // 
             // label2
@@ -91,62 +95,76 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label2.Location = new System.Drawing.Point(132, 260);
+            this.label2.Location = new System.Drawing.Point(20, 183);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 28);
+            this.label2.Size = new System.Drawing.Size(62, 21);
             this.label2.TabIndex = 21;
-            this.label2.Text = "Course";
+            this.label2.Text = "Course:";
             // 
-            // CourseSelectBox1
+            // CourseSelectBox
             // 
-            this.CourseSelectBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.CourseSelectBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CourseSelectBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.CourseSelectBox1.FormattingEnabled = true;
-            this.CourseSelectBox1.Location = new System.Drawing.Point(132, 290);
-            this.CourseSelectBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.CourseSelectBox1.Name = "CourseSelectBox1";
-            this.CourseSelectBox1.Size = new System.Drawing.Size(480, 36);
-            this.CourseSelectBox1.TabIndex = 20;
+            this.CourseSelectBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.CourseSelectBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CourseSelectBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.CourseSelectBox.FormattingEnabled = true;
+            this.CourseSelectBox.Location = new System.Drawing.Point(20, 210);
+            this.CourseSelectBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.CourseSelectBox.Name = "CourseSelectBox";
+            this.CourseSelectBox.Size = new System.Drawing.Size(361, 29);
+            this.CourseSelectBox.TabIndex = 20;
             // 
             // Choose
             // 
             this.Choose.Font = new System.Drawing.Font("Segoe UI", 12F);
             this.Choose.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.Choose.Location = new System.Drawing.Point(191, 367);
+            this.Choose.Location = new System.Drawing.Point(20, 260);
+            this.Choose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Choose.Name = "Choose";
-            this.Choose.Size = new System.Drawing.Size(339, 36);
+            this.Choose.Size = new System.Drawing.Size(361, 37);
             this.Choose.TabIndex = 22;
             this.Choose.Text = "Choose";
             this.Choose.UseVisualStyleBackColor = true;
+            this.Choose.Click += new System.EventHandler(this.OnCourseSelect);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Choose);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.CourseSelectBox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.UniversitySelectBox);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.FacultySelectBox);
+            this.panel1.Location = new System.Drawing.Point(84, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(401, 319);
+            this.panel1.TabIndex = 23;
             // 
             // PopupCourseComparison
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 478);
-            this.Controls.Add(this.Choose);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.CourseSelectBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.UniversitySelectBox);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.FacultySelectBox1);
+            this.ClientSize = new System.Drawing.Size(600, 388);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "PopupCourseComparison";
             this.Text = "PopupCourseComparison";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox FacultySelectBox1;
+        private System.Windows.Forms.ComboBox FacultySelectBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox UniversitySelectBox;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox CourseSelectBox1;
+        private System.Windows.Forms.ComboBox CourseSelectBox;
         private System.Windows.Forms.Button Choose;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- View/PopupCourseComparison.cs
+++ View/PopupCourseComparison.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Advisor.View
@@ -15,6 +8,11 @@ namespace Advisor.View
         public PopupCourseComparison()
         {
             InitializeComponent();
+        }
+
+        private void OnCourseSelect(object sender, EventArgs e)
+        {
+
         }
     }
 }
