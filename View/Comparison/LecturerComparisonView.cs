2a81ec4f288fa341d607be5fdee132c1623d2522
povilassta <povilas1999@gmail.com>
2019-10-05 13:29:36 +0300

    Added Lecturer Comparison View

---
+++ View/Comparison/LecturerComparisonView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Advisor.View.Comparison
+{
+    public partial class LecturerComparisonView : Form
+    {
+        public LecturerComparisonView()
+        {
+            InitializeComponent();
+        }
+
+        private void label19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
