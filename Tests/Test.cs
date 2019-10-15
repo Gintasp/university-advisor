d5cff6609cb5911c6fbffc0e133d5607b78e637b
Gintautas <35382935+Gintasp@users.noreply.github.com>
2019-10-15 18:21:52 +0300

    Install xUnit and write demo test (#63)

---
+++ Tests/Test.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Advisor.Tests
+{
+    public class DemoTest
+    {
+        [Fact]
+        public void DemoPassingTest()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
