3ccda78ed0452d6a10e7bdf660b38bdce2f568d8
Gintautas Plonis <gplonis@gmail.com>
2019-10-15 19:33:38 +0300

    Code style improvements

--- Service/Validator/SignupDataValidator.cs
+++ Service/Validator/SignupDataValidator.cs
@@ -21,27 +21,31 @@ namespace Advisor.Service.Validator
 
             return false;
         }
+
         public string GetSignupDataVadilatorErrorMessage()
         {
             return errorMessage;
         }
+
         private bool CheckEmailByRegex()
         {
             Regex r = new Regex(@"^([\w\.]+)@([\w\-]+)((\.(\w){2,3})+)$");
             if (r.IsMatch(user.Email)) return true;
-
             errorMessage = "Invalid email.";
+
             return false;
         }
+
         private bool CheckPasswordByRegex()
         {
             Regex r = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$");
             if(r.IsMatch(user.Password)) return true;
 
             errorMessage = "Password must contain at least eight characters, one uppercase letter, one lowercase letter and one number";
-            return false;
 
+            return false;
         }
+
         private bool CheckForEmptyFields(string passConfirm)
         {
             if (!(string.IsNullOrWhiteSpace(user.Email)
@@ -50,8 +54,8 @@ namespace Advisor.Service.Validator
                 || string.IsNullOrWhiteSpace(passConfirm))) return true;
                       
              errorMessage = "Please enter all required fields.";
+
              return false;
-            
         }
     }
 }
