d45b0c8e6eece6c566d8bea6ffde4c67b06c4c8e
Gintautas Plonis <gplonis@gmail.com>
2019-10-29 20:56:13 +0200

    Revert back to original.

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,5 @@
-﻿using Advisor.Models;
-using Microsoft.Owin;
+﻿using Microsoft.Owin;
 using Owin;
-using System;
-using System.IO;
-using System.Linq;
 
 [assembly: OwinStartupAttribute(typeof(Advisor.Startup))]
 namespace Advisor
@@ -12,9 +8,6 @@ namespace Advisor
     {
         public void Configuration(IAppBuilder app)
         {
-            string filePath = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).FullName + "\\Migrations\\Data\\User.csv";
-            var users = DB.Instance.Users.ToList();
-            var reviews = File.ReadAllLines(filePath).ToList();
             ConfigureAuth(app);
         }
     }
