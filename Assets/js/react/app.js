33b7125c08d4c8b2fda1b096665b9023b1ed151f
Gintautas Plonis <gplonis@gmail.com>
2019-11-01 23:02:34 +0200

    Setup react.

---
+++ Assets/js/react/app.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => (
+    <React.Fragment>
+    </React.Fragment>
+);
+
+render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+﻿"use strict";
+
+var path = require("path");
+var WebpackNotifierPlugin = require("webpack-notifier");
+var BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+
+module.exports = {
+    entry: "./Assets/js/react/app.js",
+    output: {
+        path: path.resolve(__dirname, "./Assets/js/dist/"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    devtool: "inline-source-map",
+    plugins: [new WebpackNotifierPlugin(), new BrowserSyncPlugin()]
+};
