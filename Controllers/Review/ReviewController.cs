b0d8d2a48fcf4c51a2f4d4e9b881eb9d554ee4cd
Laima Volungeviciute <laima.volungeviciute@gmail.com>
2019-11-06 20:38:20 +0200

    created view and controller
    created new empty view and review controller

---
+++ Controllers/Review/ReviewController.cs
@@ -0,0 +1,30 @@
+﻿using Advisor.Models;
+using Advisor.Services.Statistics;
+using System.Linq;
+using System;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+namespace Advisor.Controllers
+{
+    public class ReviewController: Controller, IReviewController
+    {
+        public ReviewController()
+        {
+        }
+
+        [Route("review", Name = "review_page")]
+        public ActionResult Review()
+        {
+            return View("/Views/Review.cshtml");
+        }
+
+        public void HandleSubmitReviewButtonClick()
+        {
+        }
+
+      public void SaveReview(Review review)
+        {
+        }
+    }
+}
