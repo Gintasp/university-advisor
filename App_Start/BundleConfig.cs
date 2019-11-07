f5c14bf3c1f1dafb714c26f4477a8e2a100c579c
Laima Volungeviciute <laima.volungeviciute@gmail.com>
2019-11-07 16:53:43 +0200

    Added stars and review text area
    Added stars for overall rating and text area

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -29,6 +29,7 @@ namespace Advisor
                   "~/Assets/css/comparison.css",
                   "~/Assets/css/university.css",
                   "~/Assets/css/faculty.css",
+                  "~/Assets/css/review.css",
                   "~/Assets/css/layout.css"
             ));
         }
