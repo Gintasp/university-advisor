e667c5be52c255c381c769caa4c60f8bf24375a9
Gintautas Plonis <gplonis@gmail.com>
2019-11-08 20:33:02 +0200

    Split Review.cshtml into smaller files.

--- Controllers/Review/ReviewController.cs
+++ Controllers/Review/ReviewController.cs
@@ -5,27 +5,19 @@ using System.Collections.Generic;
 
 namespace Advisor.Controllers
 {
-    public class ReviewController: Controller, IReviewController
+    public class ReviewController : Controller, IReviewController
     {
         public ReviewController()
         {
         }
 
         [Route("review", Name = "review_page")]
-        public ActionResult Review()
+        public ActionResult Index()
         {
             List<University> unis = DB.Instance.Universities.ToList();
             ViewBag.Universities = unis;
 
-            return View("/Views/Review.cshtml");
-        }
-
-        public void HandleSubmitReviewButtonClick()
-        {
-        }
-
-      public void SaveReview(Review review)
-        {
+            return View("/Views/Review/Review.cshtml");
         }
     }
 }
