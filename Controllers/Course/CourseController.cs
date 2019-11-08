13cdc8ef37c75aaebce7a50078e5293e8cae47fa
LAPTOP-CJ2L1GV8\Gabriele <gabrielesirvinskyte@gmail.com>
2019-11-08 02:14:54 +0200

    deleted old method

--- Controllers/Course/CourseController.cs
+++ Controllers/Course/CourseController.cs
@@ -51,20 +51,5 @@ namespace Advisor.Controllers
 
             return statsData;
         }
-
-        //private void LoadStats()
-        //{
-            //StatsData statsData = new StatsData();
-            //StatisticCalculator calculator = new StatisticCalculator();
-            //List<Review> courseReviews = Course.Reviews.ToList();
-            //statsData.Usefulness = calculator.CalcReviewAverage(courseReviews, r => r.Usefulness, 1);
-            //statsData.Difficulty = calculator.CalcReviewAverage(courseReviews, r => r.Difficulty, 1);
-            //statsData.Satisfaction = calculator.CalcReviewAverage(courseReviews, r => r.Satisfaction, 1);
-            //statsData.OveralRating = calculator.CalcReviewAverage(courseReviews, r => r.OveralRating, 1);
-            //statsData.Theory = calculator.CalcReviewAverage(courseReviews, r => r.TheoryPercentage, 1);
-            //statsData.Practice = calculator.CalcReviewAverage(courseReviews, r => r.PracticePercentage, 1);
-
-            //CourseView.StatsData = statsData;
-        //}
     }
 }
