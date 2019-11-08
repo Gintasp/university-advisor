285c3ec7c9456cc14e9677bf014422a02cba629e
LAPTOP-CJ2L1GV8\Gabriele <gabrielesirvinskyte@gmail.com>
2019-11-08 02:11:00 +0200

    course page

--- Controllers/Course/CourseController.cs
+++ Controllers/Course/CourseController.cs
@@ -1,13 +1,59 @@
-﻿namespace Advisor.Controllers
+﻿using Advisor.Models;
+using System;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Advisor.Services.Statistics;
+
+namespace Advisor.Controllers
 {
-    public class CourseController : ICourseController
+    public class CourseController : Controller, ICourseController
     {
-        public void LoadCourseData()
+        public IStatsBuilder StatsBuilder { get; set; }
+        public CourseController(IStatsBuilder statsBuilder)
         {
+            StatsBuilder = statsBuilder;
         }
 
-        private void LoadStats()
+        [Route("course/{id?}", Name = "course_page")]
+        public ActionResult Index(int? id)
         {
+            if (id != null)
+            {
+                Course course = DB.Instance.Courses.Where(s => s.Id == id).SingleOrDefault();
+                if (course == null)
+                {
+                    return View("/Views/Shared/404.cshtml");
+                }
+
+                ViewBag.Course = course;
+                ViewBag.StatsData = LoadStats(course);
+
+                return View("/Views/Course/Course.cshtml");
+            }
+
+            return View("/Views/Shared/404.cshtml");
+        }
+        private StatsData LoadStats(Course course)
+        {
+            var stats = StatsBuilder.BuildCourseStats(course);
+            StatsData statsData = new StatsData
+            {
+                ReviewCount = stats.review_count,
+                Satisfaction = stats.satisfaction,
+                Difficulty = stats.difficulty,
+                Usefulness = stats.usefulness,
+                Interesting = stats.interesting,
+                Theory = stats.theory,
+                Practice = stats.practice,
+                OveralRating = stats.overal
+            };
+
+            return statsData;
+        }
+
+        //private void LoadStats()
+        //{
             //StatsData statsData = new StatsData();
             //StatisticCalculator calculator = new StatisticCalculator();
             //List<Review> courseReviews = Course.Reviews.ToList();
@@ -19,6 +65,6 @@
             //statsData.Practice = calculator.CalcReviewAverage(courseReviews, r => r.PracticePercentage, 1);
 
             //CourseView.StatsData = statsData;
-        }
+        //}
     }
 }
