33e3cb7c1c13a1f6d4e9066befdfd1a8df859307
Laima Volungeviciute <laima.volungeviciute@gmail.com>
2019-11-14 20:41:54 +0200

    Added controller and partials
    Added admin controller, views and link to admin view

---
+++ Controllers/Admin/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Advisor.Controllers
+{
+    public class AdminController: Controller
+    {
+        [Route("admin", Name = "admin_page")]
+        public ActionResult Index()
+        {
+
+            return View("/Views/Admin/Admin.cshtml");
+        }
+
+    }
+}
