a69f5c05014e56a4111247480369b912f704935c
povilassta <48487014+povilassta@users.noreply.github.com>
2019-11-19 20:57:27 +0200

    Branch#101 (testai, testukai) (#95)
    * Added UploadFile method tests
    
    * Added Validate method tests.
    
    * Added StatBuilder tests
    
    * Cleanup unused references.

---
+++ Tests/FileManagerTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Web;
+using Moq;
+using Xunit;
+using Advisor.Services.IO;
+
+namespace Advisor.Tests
+{
+    public class FileManagerTests
+    {
+        [Fact]
+        public void UploadFile_ValidSize()
+        {
+            //Arrange
+            Mock<HttpPostedFileBase> fileMock = new Mock<HttpPostedFileBase>();
+            FileManager fileManager = new FileManager();
+            var expected = true;
+
+            fileMock
+                .Setup(f => f.ContentLength)
+                .Returns(10);
+
+            fileMock
+                .Setup(f => f.FileName)
+                .Returns("testimage.jpeg");
+
+            //Act
+            var actual = fileManager.UploadFile(fileMock.Object);
+            
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void UploadFile_InvalidSize()
+        {
+            //Arrange
+            Mock<HttpPostedFileBase> fileMock = new Mock<HttpPostedFileBase>();
+            FileManager fileManager = new FileManager();
+            var expected = false;
+
+            fileMock
+                .Setup(f => f.ContentLength)
+                .Returns(-1);
+
+            fileMock
+                .Setup(f => f.FileName)
+                .Returns("testimage.jpeg");
+
+            //Act
+            var actual = fileManager.UploadFile(fileMock.Object);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void UploadFile_NoName()
+        {
+            //Arrange
+            Mock<HttpPostedFileBase> fileMock = new Mock<HttpPostedFileBase>();
+            FileManager fileManager = new FileManager();
+            var expected = true;
+
+            fileMock
+                .Setup(f => f.ContentLength)
+                .Returns(50);
+
+            fileMock
+                .Setup(f => f.FileName)
+                .Returns("");
+
+            //Act
+            var actual = fileManager.UploadFile(fileMock.Object);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Tests/FileValidatorTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Web;
+using Advisor.Services.Validator;
+using Moq;
+using Xunit;
+
+namespace Advisor.Tests
+{
+    public class FileValidatorTests
+    {
+        [Theory]
+        [InlineData("image/jpeg")]
+        [InlineData("image/png")]
+        [InlineData("application/pdf")]
+        [InlineData("text/plain")]
+        [InlineData("application/vnd.openxmlformats-officedocument.wordprocessingml.document")]
+        public void Validate_ValidType(string contentType)
+        {
+            //Arrange
+            var expected = true;
+            FileValidator fileValidator = new FileValidator();
+            Mock<HttpPostedFileBase> fileMock = new Mock<HttpPostedFileBase>();
+            fileMock
+                .Setup(f => f.ContentType)
+                .Returns(contentType);
+
+            //Act
+            var actual = fileValidator.Validate(fileMock.Object);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Validate_InvalidType()
+        {
+            //Arrange
+            var expected = false;
+            FileValidator fileValidator = new FileValidator();
+            Mock<HttpPostedFileBase> fileMock = new Mock<HttpPostedFileBase>();
+            fileMock
+                .Setup(f => f.ContentType)
+                .Returns("image/jpg");
+
+            //Act
+            var actual = fileValidator.Validate(fileMock.Object);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
