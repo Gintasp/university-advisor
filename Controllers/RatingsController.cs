f2d4befe3fc3bc2a801475486851c00806ea4605
Laima Volungeviciute <laima.volungeviciute@gmail.com>
2019-11-29 14:09:24 +0200

    added views and controller
    created new folder, added html files and a controller

---
+++ Controllers/RatingsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Advisor.Controllers
+{
+    public class RatingsController : Controller
+    {
+        // GET: Ratings
+        [Route("ratings", Name = "ratings_page")]
+        public ActionResult Index()
+        {
+            
+
+            return View("/Views/Advisor/Ratings/Ratings.cshtml");
+        }
+    }
+}
