dddb3bf4e58a5eeed3f0c239acd77ed116ade8ce
volungeviciute <47724981+volungeviciute@users.noreply.github.com>
2019-11-30 15:12:37 +0200

    Branch#130 (#103)
    * added views and controller
    
    created new folder, added html files and a controller
    
    * connected partials to Ratings view
    
    connected partials
    
    * Rating pages
    
    istryniau nereikalingus pages ir pridejau content i likusius du
    
    * css fixes
    
    pridetas breakpointas
    
    * Minor style improvements.

---
+++ Controllers/RatingsController.cs
@@ -0,0 +1,22 @@
+﻿using Advisor.Models;
+using Advisor.Services.Statistics;
+using System.Linq;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+
+namespace Advisor.Controllers
+{
+    public class RatingsController : Controller
+    {
+        // GET: Ratings
+        [Route("ratings", Name = "ratings_page")]
+        public ActionResult Index()
+        {
+            List<University> unis = DB.Instance.Universities.ToList();
+            ViewBag.Universities = unis;
+
+            return View("/Views/Advisor/Ratings/Ratings.cshtml");
+        }
+    }
+}
