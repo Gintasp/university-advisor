a4a7f897c0d6f13bdb82bc3f9896065f3a267640
povilassta <48487014+povilassta@users.noreply.github.com>
2019-12-11 16:11:18 +0200

    Feature/search (#111)
    * Added basic search functionality
    
    * style improvements

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Advisor.Models;
+
+namespace Advisor.Controllers
+{
+    public class SearchController : Controller
+    {
+        [HttpPost]
+        [Route("search/{searchString}", Name = "search_page")]
+        public ActionResult Index(string searchString)
+        {
+            var unis = from u in DB.Instance.Universities
+                       select u;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                unis = unis.Where(s => s.Title.Contains(searchString));
+            }
+
+            ViewBag.Universities = unis.ToList();
+            ViewBag.SearchPhrase = searchString;
+
+            return View("/Views/Advisor/Search.cshtml");
+        }
+
+        public ActionResult SearchResult(string searchString)
+        {
+            return Index(searchString);
+        }
+    }
+}
